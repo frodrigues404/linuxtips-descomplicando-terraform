5518637ea5f7bceb9754925abef6ecdfe912cdd6
Fernando <fernando.rodrigues@compasso.com.br>
2022-08-25 15:35:03 -0300

    Initial commit

---
+++ day01/terraform.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+provider "aws" {
+  alias  = "west"
+  region = "us-west-1"
+}
+
+# terraform {
+#   backend "s3" {
+#     bucket = "fernando.rodrigues-tfstates"
+#     key    = "day01/terraform.tfstate"
+#     region = "us-east-1"
+#   }
+# }
